e8c7a4cb1c31c177fba434ce80d8a44d141f631a
Karen Caplan <keaslk@gmail.com>
2013-01-07 21:48:56 -0800

    adding classes to game.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,15 +1,38 @@
-def time
-  current_time = Time.new
-  current_time.strftime("%r")
+class Player
+
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health  
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+
+  def to_s
+    "Hello, my name is #{@name} and I have a health of #{@health}"
+  end
+
+
 end
 
-def say_hello(name, health=100)
-  "As of #{time} #{name.capitalize} has a health of #{health}"
-end
 
+player1 = Player.new("moe")
 
+player2 = Player.new("Larry", 60)
 
-puts say_hello("larry", 60)
-puts say_hello("curly", 125)
-puts say_hello("moe")
-puts say_hello("shemp", 90)
+player3 = Player.new("Curly", 125)
+
+puts player1
+puts player2
+puts player3
+
+player3.blam
+player2.w00t
+
